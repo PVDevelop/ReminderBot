25ca3bac59b67c9cfd8377d2aa818c6fd5d51245
Vetrov Dmitry <dim4kk@yandex.ru>
2017-02-02 23:55:38 +0300

    Rest дергалка

---
+++ root/src/DeliveryService/src/RestApi/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        [HttpPost]
+        public IActionResult SendMessage(string clientId, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
