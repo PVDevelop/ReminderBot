726c8a5c81008e8c35671f4247538080a5f7987f
Vetrov Dmitry <dim4kk@yandex.ru>
2017-02-07 23:59:29 +0300

    lib

---
+++ root/src/DeliveryService/DeliveryLib.Tests/DeliveryLibTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using DeliveryLib;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeliveryLib.Tests
+{
+    [TestFixture]
+    public class DeliveryLibTest
+    {
+        [SetUp]
+        public void Init()
+        {
+
+        }
+
+        [Test]
+        public void test1()
+        {
+
+        }
+    }
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ root/src/DeliveryService/src/DeliveryLib/DeliveryMessage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace DeliveryLib
+{
+    public class DeliveryMessage :
+        IDeliveryMessage
+    {
+        private readonly string _apiUrl = null;
+        private readonly string _token = null;
+        private readonly TimeSpan _pollingTimeout = TimeSpan.FromMinutes(1);
+
+        public DeliveryMessage(
+            string apiUrl,
+            string token)
+        {
+            _apiUrl = apiUrl;
+        }
+
+        public async Task SendMessageAsync(string chatId, string message, CancellationToken cancellationToken)
+        {
+            var parameters = new Dictionary<string, object>()
+            {
+                { "chat_id", chatId },
+                { "text", message }
+            };
+
+            var uri = new Uri($"{_apiUrl}{_token}/sendMessage");
+            var httpClientHandler = new HttpClientHandler();
+            using (var client = new HttpClient(httpClientHandler))
+            {
+                var payload = JsonConvert.SerializeObject(parameters);
+                var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
+                var response = await client
+                    .PostAsync(uri, httpContent, cancellationToken)
+                    .ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
